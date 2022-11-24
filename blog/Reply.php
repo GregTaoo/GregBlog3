2f040b6b183ffdad70f04ada480577b0dc27f9f8
GregTaoo <gregtaoo@outlook.com>
2022-11-24 16:42:18 +0800

    emotions selector!

--- blog/Reply.php
+++ blog/Reply.php
@@ -88,7 +88,7 @@ class Reply {
 
     public function parse_emotions()
     {
-        $this->text = preg_replace_callback("/\[([\u4e00-\u9fa5_a-zA-Z\d\-]*)]/", function ($res) {
+        $this->text = preg_replace_callback("/\[([\w\-]*)]/", function ($res) {
             $ret = '<img style="max-width:128px" src="';
             $config = Info::config();
             $emotions = Info::emotions();
